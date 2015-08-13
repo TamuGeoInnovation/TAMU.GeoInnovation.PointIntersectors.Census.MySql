f0351d8c33dd7f40da287c8c9956e6582705672e
austinelsik <austin_elsik@tamu.edu>
2015-08-13 11:03:26 -0500

    got point intersection working

--- src/Main/Core/MySqlPointIntersector.cs
+++ src/Main/Core/MySqlPointIntersector.cs
@@ -6,6 +6,7 @@ using USC.GISResearchLab.AddressProcessing.Core.Standardizing.StandardizedAddres
 using USC.GISResearchLab.Common.Databases.QueryManagers;
 using USC.GISResearchLab.Common.Utils.Databases;
 using TAMU.GeoInnovation.PointIntersectors.Census.PointIntersecters.AbstractClasses;
+using TAMU.GeoInnovation.PointIntersectors.Census.OutputData.CensusRecords;
 
 namespace TAMU.GeoInnovation.PointIntersectors.Census.MySql.Core
 {
@@ -32,7 +33,9 @@ namespace TAMU.GeoInnovation.PointIntersectors.Census.MySql.Core
         public override Object GetRecord(double longitude, double latitude, string table, string shapeField)
         {
 
-            DataTable ret = null;
+            IntersectionRecord ret =  new IntersectionRecord();
+            ret.Created = DateTime.Now;
+            DateTime start = DateTime.Now;
 
             try
             {
@@ -44,27 +47,62 @@ namespace TAMU.GeoInnovation.PointIntersectors.Census.MySql.Core
                     sql += " SELECT ";
                     sql += "  * ";
                     sql += " FROM ";
-                    sql += "'" + table + "'";
+                    sql += "" + table + "";
                     sql += " WHERE ";
-
-
-                    sql += "  Contains(" + shapeField + ", GeomFromText('POINT(?latitude ?longitude)')) = 1";
+                    sql += "  Contains(" + shapeField + ", GeomFromText('POINT(" + longitude + " " + latitude + ")')) = 1";
+                    //sql += "  Contains(" + shapeField + ", GeomFromText(?test)) = 1";
 
                     SqlCommand cmd = new SqlCommand(sql);
 
-                    cmd.Parameters.Add(SqlParameterUtils.BuildSqlParameter("latitude", SqlDbType.Decimal, latitude));
-                    cmd.Parameters.Add(SqlParameterUtils.BuildSqlParameter("longitude", SqlDbType.Decimal, longitude));
+                    //ODO - Come back and try to make this work with a parameterized query - http://stackoverflow.com/questions/8355000/can-i-do-a-parameterized-query-containing-geometry-function
+                    //cmd.Parameters.Add(SqlParameterUtils.BuildSqlParameter("test", SqlDbType.VarChar, "'POINT(" + longitude + " " + latitude + ")'"));
 
                     IQueryManager qm = ReferenceDataQueryManager;
-                    qm.AddParameters(cmd.Parameters);
-                    ret = qm.ExecuteDataTable(CommandType.Text, cmd.CommandText, true);
+                   // qm.AddParameters(cmd.Parameters);
+                    DataTable dataTable = qm.ExecuteDataTable(CommandType.Text, cmd.CommandText, true);
+
+                    if (dataTable != null && dataTable.Rows.Count > 0)
+                    {
+                        if (dataTable.Rows.Count == 1)
+                        {
+                            DataRow dataRow = dataTable.Rows[0];
+                            ret = new IntersectionRecord();
+                            ret.InputLatitude = latitude;
+                            ret.InputLongitude = longitude;
+                            ret.MatchedId = Convert.ToString(dataRow["geoId10"]);
+
+                            ret.FieldNames = new string[dataTable.Columns.Count];
+                            ret.FieldValues = new object[dataTable.Columns.Count];
+                            ret.FieldTypes = new Type[dataTable.Columns.Count];
+
+                            for (int i=0; i< dataTable.Columns.Count; i++)
+                            {
+                                DataColumn column = dataTable.Columns[i];
+
+                                ret.FieldNames[i] = column.ColumnName;
+                                ret.FieldValues[i] = dataRow[column.ColumnName];
+                                ret.FieldTypes[i] = column.DataType;
+                            }
+
+                        }
+                        else
+                        {
+                            throw new Exception("Returned more than one matching geography - matching count:" + dataTable.Rows.Count);
+                        }
+                    }
+
                 }
             }
             catch (Exception e)
             {
+                ret.Exception = e;
+                ret.ExceptionOccurred = true;
                 throw new Exception("Exception occurred GetRecord: " + e.Message, e);
             }
 
+            DateTime end = DateTime.Now;
+            TimeSpan duration = end.Subtract(start);
+            ret.TimeTaken = duration.TotalMilliseconds;
             return ret;
         }
 
